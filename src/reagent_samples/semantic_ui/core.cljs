0801ac4b1d58122e8b15d5b8d0865290f88c56c2
makinoshi <masa.kinoshita02@gmail.com>
2019-04-22 13:00:26 +0900

    Add more semantic ui components

--- src/reagent_samples/semantic_ui/core.cljs
+++ src/reagent_samples/semantic_ui/core.cljs
@@ -3,7 +3,8 @@
             [goog.object :as go]
             [goog.dom :as gd]
             [goog.dom.TagName :as tag]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent])
+  (:refer-clojure :exclude [list]))
 
 ;; Semantic UI Reagent components
 
@@ -37,6 +38,38 @@
 
 (def label (->c :Label))
 
+(def list (->c :List))
+
+(def list-item (->c :List :Item))
+
+(def list-icon (->c :List :Icon))
+
+(def list-content (->c :List :Content))
+
+(def list-header (->c :List :Header))
+
+(def list-description (->c :List :Description))
+
+(def dimmer (->c :Dimmer))
+
+(def loader (->c :Loader))
+
+(def placeholder (->c :Placeholder))
+
+(def placeholder-header (->c :Placeholder :Header))
+
+(def placeholder-line (->c :Placeholder :Line))
+
+(def placeholder-paragraph (->c :Placeholder :Paragraph))
+
+(def modal (->c :Modal))
+
+(def modal-header (->c :Modal :Header))
+
+(def modal-content (->c :Modal :Content))
+
+(def modal-description (->c :Modal :Description))
+
 ;; Component adapted Reagent components
 
 (defn- components-using-adapt-react-class []
@@ -64,7 +97,9 @@
       [:p "4 5 6"]]]
     [divider {:vertical true} "AND"]]
    [segment
-    [flag {:name "jp"}]]
+    [flag {:name "jp"}]
+    [flag {:name "us"}]
+    [flag {:name "cn"}]]
    [segment
     [input {:placeholder "sample input"}]
     [input {:loading true :icon "user"}]
@@ -72,14 +107,64 @@
    [segment
     [label "foo"]
     [label [icon {:name "mail"}] "Send"]
-    [label {:as "a" :tag true :color "teal"} "TAG"]]])
+    [label {:as "a" :tag true :color "teal"} "TAG"]]
+   [segment
+    [list {:divided true :relaxed true}
+     [list-item
+      [list-icon {:name "github" :size "large" :verticalAlign "middle"}]
+      [list-content
+       [list-header {:as "a" :href "https://github.com/Semantic-Org/Semantic-UI" :target "_blank"}
+        "Semantic-Org/Semantic-UI"]
+       [list-description "Using this CSS framework"]]]]]
+   [segment
+    [dimmer {:active true :inverted false}
+     [loader "Loading.."]]
+    [:p "hoge hoge hoge"]
+    [:p "fuga fuga fuga"]]
+   [segment
+    [placeholder
+     [placeholder-header {:image true}
+      [placeholder-line]
+      [placeholder-line]]
+     [placeholder-paragraph
+      [placeholder-line]
+      [placeholder-line]
+      [placeholder-line]]]]
+   [segment
+    [modal {:trigger (reagent/as-element [button "Show Modal"])}
+     [modal-header "Modal header"]
+     [modal-content
+      "foo"
+      [modal-description
+       [header "bar"]
+       [:p "hoge"]]]]]])
 
 ;; Component using syntax sugar
 
 (defn- components-using-shortern-syntax []
   [:> sui/Container {:style {:margin-top "5em"}}
    [:> sui/Header {:as "h1"}
-    "Show components using " [:code "[:> ... ]"] " syntac"]])
+    "Show components using " [:code "[:> ... ]"] " syntac"]
+   [:> sui/Segment {:placeholder true}
+    [:> sui/Header {:icon true}
+     [:> sui/Icon {:name "search"}]
+     "We don't have any documents matching your query."]
+    [:> sui/Segment.Inline
+     [:> sui/Button {:primary true} "Color Query"]
+     [:> sui/Button "Add document"]]]
+   [:> sui/Segment
+    [:> sui/Step.Group {:ordered true}
+     [:> sui/Step {:completed true}
+      [:> sui/Step.Content
+       [:> sui/Step.Title "Shipping"]
+       [:> sui/Step.Description "Choose your shipping options"]]]
+     [:> sui/Step {:completed true}
+      [:> sui/Step.Content
+       [:> sui/Step.Title "Billing"]
+       [:> sui/Step.Description "Enter billing information"]]]
+     [:> sui/Step {:active true}
+      [:> sui/Step.Content
+       [:> sui/Step.Title "Confirm Order"]]]]]])
 
 (defn- back-link []
   [container {:style {:margin-top "2em"}}
